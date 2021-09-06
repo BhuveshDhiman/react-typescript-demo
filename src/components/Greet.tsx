45915763fe6916a700aad0a71a28c1e8fbb51502
Bhuvesh Dhiman <67685811+BhuveshDhiman@users.noreply.github.com>
2021-09-06 20:57:24 +0530

    Intro to types

---
+++ src/components/Greet.tsx
@@ -0,0 +1,12 @@
+
+type GreetProps = {
+	name: string
+}
+
+export const Greet = (props: GreetProps) => {
+	return (
+		<div>
+			<h2>Welcome {props.name}! You have 7 unread messages</h2>
+		</div>
+	)
+}
