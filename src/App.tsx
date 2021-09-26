4edd924c8c5f313a51b3256491ccd2a629852a89
Bhuvesh Dhiman <67685811+BhuveshDhiman@users.noreply.github.com>
2021-09-26 20:11:56 +0530

    Template literals

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import { Private } from './components/auth/Private'
 import { Profile } from './components/auth/Profile'
 import { List } from './components/generics/List'
 import { RandomNumber } from './components/restriction/RandomNumber'
+import { Toast } from "./components/templateliterals/Toast"
 
 
 function App() {
@@ -70,7 +71,8 @@ function App() {
 					first: "John", last: "Doe"
 				}
 			]} onClick={(item) => console.log(item)} /> */}
-			<RandomNumber value={10} isPositive />
+			{/* <RandomNumber value={10} isPositive /> */}
+			<Toast position='center' />
 		</div>
 	);
 }
